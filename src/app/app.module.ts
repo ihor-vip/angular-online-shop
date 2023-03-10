def05ae194210e446eda49d85ea50f7f3c331f07
ihor-vip <vip20140902@gmail.com>
2023-03-10 11:58:44 +0200

    create all components for app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/ui/header/header.component';
+import { FooterComponent } from './components/ui/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
+import { BasketComponent } from './components/basket/basket.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { BaseComponent } from './components/base/base.component';
+import { DialogBoxComponent } from './components/dialog-box/dialog-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent,
+    BasketComponent,
+    ProductDetailsComponent,
+    BaseComponent,
+    DialogBoxComponent
   ],
   imports: [
     BrowserModule,
